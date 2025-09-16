748f170d28597c8efb78e7fe3be23660be749ca2
c0ulday <mamasitaurslut@gmail.com>
2025-09-16 13:01:36 +0200

    ajout liens email stmp

--- Internet/views.py
+++ Internet/views.py
@@ -53,3 +53,47 @@ def monEspace(request):
 
 def volsDecouverte(request):
     return render(request,'volsdecouverte.html',context={})
+
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+
+def fomulaireDecouverte(request):
+    
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+
+        # Contenu du mail
+        subject = f"Nouveau message de {name} via le formulaire de contact"
+        message_body = f"""
+        Nom : {name}
+        Téléphone : {phone}
+        E-mail : {email}
+
+        Message :
+        {message}
+        """
+
+        try:
+            send_mail(
+                subject,
+                message_body,
+                settings.DEFAULT_FROM_EMAIL,  # l’expéditeur configuré
+                [settings.CONTACT_EMAIL],   # le destinataire configuré
+                fail_silently=False,
+            )
+            messages.success(request, "Votre message a été envoyé avec succès !")
+        except Exception as e:
+            messages.error(request, f"Erreur lors de l'envoi du message : {e}")
+
+        return redirect('vols_decouverte')  # redirige vers la même page
+
+    return render(request, 'volsDecouverte.html')
+
+
+def navigation(request):
+    return render(request,'navigation.html',context={})
